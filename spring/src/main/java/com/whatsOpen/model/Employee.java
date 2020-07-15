7c4796e6d53e44ae5e64eeb6e69c12bd8a9da7ac
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-07-15 10:02:43 -0400

    added the id get/set

--- spring/src/main/java/com/whatsOpen/model/Employee.java
+++ spring/src/main/java/com/whatsOpen/model/Employee.java
@@ -22,12 +22,22 @@ public class Employee {
 	public Employee() {
 		
 	}
-	public Employee(String firstName, String lastName, String email, String password) {
+	public Employee(Integer id, String firstName, String lastName, String email, String password) {
 		super();
+		this.id = id;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
+	}
+	
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
 	}
 	public String getFirstName() {
 		return firstName;
