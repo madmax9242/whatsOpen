ffcbaf25fc53912c83a8a761da2f975616286902
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-07-15 10:06:25 -0400

    didnt need to have in constructor

--- spring/src/main/java/com/whatsOpen/model/Employee.java
+++ spring/src/main/java/com/whatsOpen/model/Employee.java
@@ -22,16 +22,12 @@ public class Employee {
 	public Employee() {
 		
 	}
-	public Employee(Integer id, String firstName, String lastName, String email, String password) {
-		super();
-		this.id = id;
+	public Employee(String firstName, String lastName, String email, String password) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
 		this.password = password;
 	}
-	
-	
 	
 	public Integer getId() {
 		return id;
