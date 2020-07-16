b1078357cd9f4db51780a9f22cb23ab7f44dd711
TK Tran <tktrannn@live.com>
2020-07-16 13:58:12 -0400

    Adding filtering and sorting.

--- angular/whatsOpen/src/app/components/schedule-list/schedule-list.component.ts
+++ angular/whatsOpen/src/app/components/schedule-list/schedule-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 import { Employee } from '../../classes/employee';
 
@@ -33,11 +34,22 @@ export class ScheduleListComponent implements OnInit {
 	constructor() { }
 
 	ngOnInit(): void {
-		this.dataSource.paginator = this.paginator; // links paginator to data source
+		this.dataSource.paginator = this.paginator; // links our paginator reference to data source
+		this.dataSource.sort = this.sort; // links our sort reference to data source
 	}
 
 	displayedColumns: string[] = ['id', 'firstName', 'lastName', 'email', 'password', 'employeeScheduleId'];
 	dataSource = new MatTableDataSource<Employee>(EMP_DATA);
 
-	@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator; // scans for paginator in HTML
+	// Pagination
+	@ViewChild(MatPaginator, { static: true }) paginator: MatPaginator; // scans for paginator in HTML and assigns to reference variable paginator
+
+	// Sorting
+	@ViewChild(MatSort, { static: true }) sort: MatSort; // scans for sort in HTML and assigns to reference variable sort
+
+	// Filtering
+	applyFilter(event: Event) {
+		const filterValue = (event.target as HTMLInputElement).value;
+		this.dataSource.filter = filterValue.trim().toLowerCase();
+	}
 }
