6b03269ef3fdb0d95be227c7dcae2735818f5dcb
DESKTOP-M359TO2 <maxd924@gmail.com>
2020-07-21 12:41:50 -0400

    switched back to employee_id

--- angular/whatsOpen/src/app/classes/employee-schedule.ts
+++ angular/whatsOpen/src/app/classes/employee-schedule.ts
@@ -3,13 +3,9 @@ import { Employee } from "./employee";
 export class EmployeeSchedule {
 
     id: number;
-    monday: string;
-    tuesday: string;
-    wednesday: string;
-    thursday: string;
-    friday: string;
-    saturday: string;
-    sunday: string;
-    employeeId: number;
+    date: string;
+    description: string;
+    shiftType: string;
+    employee: Employee;
 
 }
